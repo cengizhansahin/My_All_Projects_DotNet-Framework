31ae350f97837e8e53fdc77e000f2c129278f40c
Cengizhan <cengizhan7sahin@gmail.com>
2023-12-27 19:59:19 +0300

    class ve database sınıfı oluşturuldu ve  connection string eklendi

--- CodeFirstProject/CodeFirstProject/Program.cs
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+public class ETicaretDbContext : DbContext
+{
+    public DbSet<Urun> uruns { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-S5SNUUO;Database=CodeFirstETicaretDb;Trusted_Connection=True;");
+    }
+}
+
+public class Urun
+{
+    public int Id { get; set; }
+    public string UrunAdi { get; set; }
+    public int MyProperty { get; set; }
+}
