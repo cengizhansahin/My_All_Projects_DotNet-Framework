46eba3adbf966f4ecbd5cb8eb17c79c549c38919
Cengizhan <cengizhan7sahin@gmail.com>
2023-12-30 12:21:08 +0300

    database class ı oluşturuldu ve veri tabnıyla bağlantı kuruldu

---
+++ KitaplikMVCStart/Kitaplik/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Kitaplik.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kitaplik.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
