9e6ec37f6b77226788fd5b02ddac8bb5903a00f3
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-06 10:28:33 +0300

    manuel olarak meetingcontroller oluşturuldu

--- MVC-vs-code/MeetingApp/Program.cs
+++ MVC-vs-code/MeetingApp/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 // request pipeline
 //authentication
 //
+//url/controller/action/id?
+// app.MapDefaultControllerRoute();
 app.MapGet("/", () => "Hello World!");
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{Id?}"
+);
 app.Run();
