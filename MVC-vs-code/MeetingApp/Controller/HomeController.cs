d06564b4421101ab48980064d439a4ec924875bb
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-06 14:16:18 +0300

    viewbag ile document yerine sayfa isimleri yazdırıldı

--- MVC-vs-code/MeetingApp/Controller/HomeController.cs
+++ MVC-vs-code/MeetingApp/Controller/HomeController.cs
@@ -9,7 +9,11 @@ namespace MeetingApp.Controllers
         {
             int saat = DateTime.Now.Hour;
             // var greeting = saat > 12 ? "İyi günler" : "Günaydın";
-            ViewBag.Selamlama = saat>12?"İyi günler":"Günaydın";
+            // ViewBag.Selamlama = saat>12?"İyi günler":"Günaydın";
+            // ViewBag.Username = "Cengizhan";
+            ViewData["Selamlama"] = saat > 12 ? "İyi Günlerss" : "Günaydınss";
+            ViewData["Username"] = "Cengizhan";
+
             return View();
         }
     }
