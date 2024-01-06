adc0727291d4bd3d04524c5f3d1d4c11fd9edf40
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-06 14:51:09 +0300

    post metoduyla başvurudaki form gönderimi basit ve amele usulü gerçekleşti

--- MVC-vs-code/MeetingApp/Controller/MeetingController.cs
+++ MVC-vs-code/MeetingApp/Controller/MeetingController.cs
@@ -8,8 +8,18 @@ namespace MeetingApp.Controllers
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Apply()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Apply(string Name,string Phone, string Email, bool WillAttend)
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Phone);
+            Console.WriteLine(Email);
+            Console.WriteLine(WillAttend);
             return View();
         }
         public IActionResult List()
