eb7662634f328b748faf98a8b5623df88cea8a35
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-09 21:52:34 +0300

    product ve category modeli, controllerı ve view leri oluşturuldu

---
+++ MVC-Task/MvcTask/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MvcTask.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
