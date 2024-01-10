4507f89472bcaed9d5e782841447f2b3827e16d1
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-10 16:45:19 +0300

    repository içerisinde create category ve product metodları oluşturuldu. ve controllerda metodlar kullanıldı

--- MVC-Task/MvcTask/Controllers/CategoryController.cs
+++ MVC-Task/MvcTask/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MvcTask.Models;
 
 namespace MvcTask.Controllers
 {
@@ -12,7 +13,17 @@ namespace MvcTask.Controllers
     {
         public IActionResult Index()
         {
+            return View(Repository.Categories);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            Repository.CreateCategory(category);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Error()
--- MVC-Task/MvcTask/Models/Repository.cs
+++ MVC-Task/MvcTask/Models/Repository.cs
@@ -5,9 +5,17 @@ using System.Threading.Tasks;
 
 namespace MvcTask.Models
 {
-    public class Repository
+    public static class Repository
     {
-        public List<Category> Categories { get; set; } = new List<Category>();
-        public List<Product> Products { get; set; } = new List<Product>();
+        public static List<Category> Categories { get; set; } = new List<Category>();
+        public static List<Product> Products { get; set; } = new List<Product>();
+        public static void CreateCategory(Category category)
+        {
+            Categories.Add(category);
+        }
+        public static void CreateProduct(Product product)
+        {
+            Products.Add(product);
+        }
     }
 }
