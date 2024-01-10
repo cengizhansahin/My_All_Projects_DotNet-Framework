c3f7eaec7b5e80cc1b30a3ac1325605422a0b08f
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-10 20:24:56 +0300

    seacrhbar ile filtreleme yapıldı

--- MVC-Task/ProductFormMvcApp/Controllers/HomeController.cs
+++ MVC-Task/ProductFormMvcApp/Controllers/HomeController.cs
@@ -6,9 +6,14 @@ namespace ProductFormMvcApp.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    public IActionResult Index(string searchString)
     {
-        return View(Repository.Products);
+        var products = Repository.Products;
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            products = products.Where(p => p.Name.ToLower().Trim().Contains(searchString)).ToList();
+        }
+        return View(products);
     }
 
     public IActionResult Privacy()
