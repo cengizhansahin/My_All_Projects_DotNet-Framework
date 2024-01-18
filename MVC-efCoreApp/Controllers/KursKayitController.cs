2c7c6fbcf7d31e88834c237d21d4f16bfba22fa7
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-18 21:46:34 +0300

    kurs kayit için create sayfası oluşturuldu ve asp-item, new selectlist ile dropdown menü oluşturuldu

--- MVC-efCoreApp/Controllers/KursKayitController.cs
+++ MVC-efCoreApp/Controllers/KursKayitController.cs
@@ -4,18 +4,40 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using MVC_efCoreApp.DATA;
 
 namespace MVC_efCoreApp.Controllers
 {
-    [Route("[controller]")]
     public class KursKayitController : Controller
     {
-        
-        
-        public IActionResult Index()
+
+        private readonly DataContext _context;
+
+        public KursKayitController(DataContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
             return View();
-        }        
+        }
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Ogrenciler = new SelectList(await _context.Ogrenciler.ToListAsync(), "OgrenciId", "AdSoyad");
+            ViewBag.Kurslar = new SelectList(await _context.Kurslar.ToListAsync(), "KursId", "Baslik");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(KursKayit model)
+        {
+            model.KayitTarihi = DateTime.Now;
+            _context.KursKayitlari.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
--- MVC-efCoreApp/DATA/Ogrenci.cs
+++ MVC-efCoreApp/DATA/Ogrenci.cs
@@ -13,6 +13,13 @@ namespace MVC_efCoreApp.DATA
         public int OgrenciId { get; set; }
         public string? OgrenciAd { get; set; }
         public string? OgrenciSoyadi { get; set; }
+        public string AdSoyad
+        {
+            get
+            {
+                return this.OgrenciAd + " " + this.OgrenciSoyadi;
+            }
+        }
         public string? Eposta { get; set; }
         public string? Telefon { get; set; }
         public int MyProperty { get; set; }
