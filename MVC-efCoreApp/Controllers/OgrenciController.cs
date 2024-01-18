bb9571977de8387b30c0317f041dc204b9baf09b
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-18 17:17:55 +0300

    update işlemide yapıldı

--- MVC-efCoreApp/Controllers/OgrenciController.cs
+++ MVC-efCoreApp/Controllers/OgrenciController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MVC_efCoreApp.DATA;
 
@@ -33,8 +34,27 @@ namespace MVC_efCoreApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var ogrenciler = _context.Ogrenciler.ToList();
+            var ogrenciler = await _context.Ogrenciler.ToListAsync();
             return View(ogrenciler);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleteOgrneci = await _context.Ogrenciler.FirstOrDefaultAsync(p => p.OgrenciId == id);
+            _context.Ogrenciler.Remove(deleteOgrneci);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var updateOgrenci = await _context.Ogrenciler.FirstOrDefaultAsync(p => p.OgrenciId == id);
+            return View(updateOgrenci);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Ogrenci obj)
+        {
+            _context.Ogrenciler.Update(obj);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
