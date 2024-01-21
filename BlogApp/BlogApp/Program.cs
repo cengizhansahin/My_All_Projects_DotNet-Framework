91f6d9763fe94eec4cbb09e9f051b63a85f196ce
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-21 11:51:23 +0300

    proje oluşturuldu ve veri tabanı bağlantısı kuruldu

---
+++ BlogApp/BlogApp/Program.cs
@@ -0,0 +1,15 @@
+using BlogApp;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("sql_connection");
+    options.UseSqlite(connectionString);
+});
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
