f0c85406b851e3ffe4192e30322af734063a4289
Cengizhan <cengizhan7sahin@gmail.com>
2024-01-24 20:34:13 +0300

    postlara yorum ekleme işlemi yapıldı

--- BlogApp/BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -24,19 +24,19 @@ namespace BlogApp.Data.Concrete.EfCore
                 if (!context.Tags.Any())
                 {
                     context.Tags.AddRange(
-                        new Tag { Text = "web-programlama", Url = "web-programlama" },
-                        new Tag { Text = "back-end", Url = "back-end" },
-                        new Tag { Text = "front-end", Url = "front-end" },
-                        new Tag { Text = "full-stack", Url = "full-stack" },
-                        new Tag { Text = "react", Url = "react" }
+                        new Tag { Text = "web-programlama", Url = "web-programlama", Color = TagColors.secondary },
+                        new Tag { Text = "back-end", Url = "back-end", Color = TagColors.warning },
+                        new Tag { Text = "front-end", Url = "front-end", Color = TagColors.success },
+                        new Tag { Text = "full-stack", Url = "full-stack", Color = TagColors.primary },
+                        new Tag { Text = "react", Url = "react", Color = TagColors.info }
                     );
                     context.SaveChanges();
                 }
                 if (!context.Users.Any())
                 {
                     context.Users.AddRange(
-                        new User { UserName = "bciga" },
-                        new User { UserName = "etkin" }
+                        new User { UserName = "bciga", Image = "p1.jpg" },
+                        new User { UserName = "etkin", Image = "p2.jpg" }
                     );
                     context.SaveChanges();
                 }
@@ -52,7 +52,11 @@ namespace BlogApp.Data.Concrete.EfCore
                             Tags = context.Tags.Take(3).ToList(),
                             Image = "1.jpg",
                             UserId = 1,
-                            Url = "asp-net-core"
+                            Url = "asp-net-core",
+                            Comments = new List<Comment>{
+                                new Comment{Text="Ben bu kurstan çok faydalandım. Ürün beklediğim gibi çıktı.",PublishedOn=DateTime.Now.AddMonths(-5).AddHours(4),UserId=1},
+                                new Comment{Text="Ben çok anlamadım.",PublishedOn=DateTime.Now.AddDays(-1),UserId=2}
+                            }
                         },
                          new Post
                          {
@@ -77,16 +81,16 @@ namespace BlogApp.Data.Concrete.EfCore
                              Url = "mongo-db"
                          },
                          new Post
-                        {
-                            Title = "MS SQL",
-                            Content = "MS SQL dersleri",
-                            IsActive = true,
-                            PublishedOn = DateTime.Now.AddDays(-4),
-                            Tags = context.Tags.Take(2).ToList(),
-                            Image = "1.jpg",
-                            UserId = 1,
-                            Url = "asp-net-core"
-                        },
+                         {
+                             Title = "MS SQL",
+                             Content = "MS SQL dersleri",
+                             IsActive = true,
+                             PublishedOn = DateTime.Now.AddDays(-4),
+                             Tags = context.Tags.Take(2).ToList(),
+                             Image = "1.jpg",
+                             UserId = 1,
+                             Url = "asp-net-core"
+                         },
                          new Post
                          {
                              Title = "React Native",
@@ -113,6 +117,7 @@ namespace BlogApp.Data.Concrete.EfCore
                     );
                     context.SaveChanges();
                 }
+
             }
         }
     }
